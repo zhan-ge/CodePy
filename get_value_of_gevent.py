4600de73ec9ca61603d331cd515cf691bb7020ef
张志广 <zhange.zzg@gmail.com>
2015-06-05 16:49:23 +0800

    Gevent: get value form greenlet.

---
+++ get_value_of_gevent.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    Project:
+    Purpose: 
+    Version:
+    Author:  ZG
+    Date:    15/6/5
+"""
+
+import gevent.monkey
+gevent.monkey.patch_socket()
+
+import gevent
+from gevent import Greenlet
+
+import urllib2
+import simplejson as json
+
+def fetch(pid):
+    response = urllib2.urlopen('http://time.jsontest.com')
+    result = response.read()
+    json_result = json.loads(result)
+    datetime = json_result['time']
+
+    print('Process %s: %s' % (pid, datetime))
+    return json_result['time']
+
+def synchronous():
+    for i in range(1, 10):
+        fetch(i)
+
+def asynchronous():
+    threads = [Greenlet.spawn(fetch, i) for i in range(10)]
+    gevent.joinall(threads)
+    print [thread.value for thread in threads]
+
+print('Synchronous:')
+synchronous()
+
+print('Asynchronous:')
+asynchronous()
